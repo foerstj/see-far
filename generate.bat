3547a140a64b8447c633dcb5e0bc905476bd0618
Johannes Förstner <johannes@famfoerstner.de>
2025-02-23 13:35:25 +0100

    Add scheme to copy & edit config gas files

--- generate.bat
+++ generate.bat
@@ -2,13 +2,18 @@
 set bits=%~dp0.
 
 :: param
-set edit=%1
-echo %edit%
-if "%edit%"=="" (
-  set edit=1.2:1.25:1.3
+set mood_edit=%1
+echo %mood_edit%
+if "%mood_edit%"=="" (
+  set mood_edit=1.2:1.25:1.3
 )
 
 pushd "%GasPy%"
-venv\Scripts\python -m landscaping.edit_moods --bits "%bits%" --src git-ignore\moods --edit see-far:%edit%
+venv\Scripts\python -m landscaping.edit_moods --bits "%bits%" --src git-ignore\moods --edit see-far:%mood_edit%
+if %errorlevel% neq 0 pause
+
+robocopy "%bits%\git-ignore\config" "%bits%\config" /S
+venv\Scripts\python -m gas.edit_gas --bits "%bits%" config/engine
+venv\Scripts\python -m gas.edit_gas --bits "%bits%" config/global_settings
 if %errorlevel% neq 0 pause
 popd
