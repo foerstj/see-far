6b057789ba35a0644a79312a9a7d0da784248390
Johannes Förstner <johannes@famfoerstner.de>
2025-02-23 10:58:44 +0100

    Add repo script to build all 3 main variants

---
+++ build-release.bat
@@ -0,0 +1,5 @@
+call .\generate.bat 1.20:1.25:1.3 && call .\build.bat  20
+call .\generate.bat 1.45:1.55:1.6 && call .\build.bat  50
+call .\generate.bat 1.85:1.95:2.0 && call .\build.bat 100
+
+call .\generate.bat
