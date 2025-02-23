d5cb246583e0826edf2d8709c2b939f51f3b52e4
Johannes Förstner <johannes@famfoerstner.de>
2025-02-23 10:16:30 +0100

    More specific mood edit

--- generate.bat
+++ generate.bat
@@ -2,6 +2,6 @@
 set bits=%~dp0.
 
 pushd "%GasPy%"
-venv\Scripts\python -m landscaping.edit_moods --bits "%bits%" --src world\global\git-ignore\moods --edit fog:near-dists:mult:1.2 fog:far-dists:mult:1.25 frustum:mult:1.3
+venv\Scripts\python -m landscaping.edit_moods --bits "%bits%" --src world\global\git-ignore\moods --edit see-far:1.2:1.25:1.3
 if %errorlevel% neq 0 pause
 popd
