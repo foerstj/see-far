82911ae1ee043656d113d2c31df09843153985df
Johannes Förstner <johannes@famfoerstner.de>
2025-02-23 14:27:45 +0100

    Edit the global_settings.gas as well

--- build-release.bat
+++ build-release.bat
@@ -1,10 +1,10 @@
-call .\generate-config.bat 144:108:20:24
+call .\generate-config.bat 144:108:20:24:85
 call .\generate-moods.bat 1.20:1.25:1.3 && call .\build.bat  20
 
-call .\generate-config.bat 180:135:20:30
+call .\generate-config.bat 180:135:20:30:100
 call .\generate-moods.bat 1.45:1.55:1.6 && call .\build.bat  50
 
-call .\generate-config.bat 180:135:20:30
+call .\generate-config.bat 180:135:20:30:100
 call .\generate-moods.bat 1.85:1.95:2.0 && call .\build.bat 100
 
 :: cleanup
--- generate-config.bat
+++ generate-config.bat
@@ -5,19 +5,20 @@ set bits=%~dp0.
 set config_edit=%1
 echo %config_edit%
 if "%config_edit%"=="" (
-  set config_edit=144:108:20:24
+  set config_edit=144:108:20:24:85
 )
-for /f "tokens=1,2,3,4 delims=:" %%a in ("%config_edit%") do (
+for /f "tokens=1,2,3,4,5 delims=:" %%a in ("%config_edit%") do (
   set ce[0]=%%a
   set ce[1]=%%b
   set ce[2]=%%c
   set ce[3]=%%d
+  set ce[4]=%%e
 )
 
 
 pushd "%GasPy%"
 robocopy "%bits%\git-ignore\config" "%bits%\config" /S
 venv\Scripts\python -m gas.edit_gas --bits "%bits%" config/engine --edit engine_settings:minimap_max_size=%ce[0]% engine_settings:default_minimap_size=%ce[1]% engine_settings:minimap_step_zoom_meters=%ce[2]% engine_settings:discovery_radius=%ce[3]%
-venv\Scripts\python -m gas.edit_gas --bits "%bits%" config/global_settings
+venv\Scripts\python -m gas.edit_gas --bits "%bits%" config/global_settings --edit global_settings:mega_map:show_border_meters=%ce[4]%
 if %errorlevel% neq 0 pause
 popd
