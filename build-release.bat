a10b51c4409910a1d4a1ebf05f85c2eedb198823
Johannes Förstner <johannes@famfoerstner.de>
2025-10-17 14:22:22 +0200

    Reduce additional builds to only what I need

--- build-release.bat
+++ build-release.bat
@@ -1,7 +1,6 @@
+:: this one's just meant for testing
 call .\generate-config.bat 120:90:25:20:75
 call .\generate-moods.bat 1:1:1:0  && call .\build.bat  0
-call .\generate-moods.bat 1:1:1:6  && call .\build.bat  0_Plus6
-call .\generate-moods.bat 1:1:1:12 && call .\build.bat  0_Plus12
 
 call .\generate-config.bat 144:108:20:24:85
 call .\generate-moods.bat 1.18:1.2:1.25:0  && call .\build.bat  20
@@ -18,9 +17,8 @@ call .\generate-moods.bat 1.9:2.0:2.1:0  && call .\build.bat  100
 call .\generate-moods.bat 1.9:2.0:2.1:6  && call .\build.bat  100_Plus6
 call .\generate-moods.bat 1.9:2.0:2.1:12 && call .\build.bat  100_Plus12
 
+:: these ones are just meant for visuals, not gameplay
 call .\generate-config.bat 180:135:20:30:100
-call .\generate-moods.bat 2.8:3.0:3.2:0  && call .\build.bat  200
-call .\generate-moods.bat 2.8:3.0:3.2:6  && call .\build.bat  200_Plus6
 call .\generate-moods.bat 2.8:3.0:3.2:12 && call .\build.bat  200_Plus12
 
 :: cleanup
