ffdb4a377b6d4f01ac01859418bebf74c10cc4a3
Johannes Förstner <johannes@famfoerstner.de>
2025-10-17 13:50:08 +0200

    Revert with camera-fog distance again, results were too different from original

--- build-release.bat
+++ build-release.bat
@@ -4,19 +4,19 @@ call .\generate-moods.bat 1:1:1:6  && call .\build.bat  0_Plus6
 call .\generate-moods.bat 1:1:1:12 && call .\build.bat  0_Plus12
 
 call .\generate-config.bat 144:108:20:24:85
-call .\generate-moods.bat 1.2:1.2:1.2:0  && call .\build.bat  20
-call .\generate-moods.bat 1.2:1.2:1.2:6  && call .\build.bat  20_Plus6
-call .\generate-moods.bat 1.2:1.2:1.2:12 && call .\build.bat  20_Plus12
+call .\generate-moods.bat 1.20:1.25:1.3:0  && call .\build.bat  20
+call .\generate-moods.bat 1.20:1.25:1.3:6  && call .\build.bat  20_Plus6
+call .\generate-moods.bat 1.20:1.25:1.3:12 && call .\build.bat  20_Plus12
 
 call .\generate-config.bat 180:135:20:30:100
-call .\generate-moods.bat 1.5:1.5:1.5:0  && call .\build.bat  50
-call .\generate-moods.bat 1.5:1.5:1.5:6  && call .\build.bat  50_Plus6
-call .\generate-moods.bat 1.5:1.5:1.5:12 && call .\build.bat  50_Plus12
+call .\generate-moods.bat 1.45:1.55:1.6:0  && call .\build.bat  50
+call .\generate-moods.bat 1.45:1.55:1.6:6  && call .\build.bat  50_Plus6
+call .\generate-moods.bat 1.45:1.55:1.6:12 && call .\build.bat  50_Plus12
 
 call .\generate-config.bat 180:135:20:30:100
-call .\generate-moods.bat 2.0:2.0:2.0:0  && call .\build.bat 100
-call .\generate-moods.bat 2.0:2.0:2.0:6  && call .\build.bat 100_Plus6
-call .\generate-moods.bat 2.0:2.0:2.0:12 && call .\build.bat 100_Plus12
+call .\generate-moods.bat 1.85:1.95:2.0:0  && call .\build.bat 100
+call .\generate-moods.bat 1.85:1.95:2.0:6  && call .\build.bat 100_Plus6
+call .\generate-moods.bat 1.85:1.95:2.0:12 && call .\build.bat 100_Plus12
 
 :: cleanup
 call .\generate-config.bat
