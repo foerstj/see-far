2bbf98f30a80556c88783608bd02fb603252cb8d
Johannes Förstner <johannes@famfoerstner.de>
2025-12-07 10:46:54 +0100

    Move moods to subdir to make space for variants

--- generate-moods.bat
+++ generate-moods.bat
@@ -7,8 +7,13 @@ echo %edit%
 if "%edit%"=="" (
   set edit=1:1:1
 )
+set source=%2
+echo %source%
+if "%source%"=="" (
+  set source=original
+)
 
 pushd "%GasPy%"
-venv\Scripts\python -m landscaping.edit_moods --bits "%bits%" --src source\moods --edit see-far:%edit%
+venv\Scripts\python -m landscaping.edit_moods --bits "%bits%" --src "source\moods\%source%" --edit see-far:%edit%
 if %errorlevel% neq 0 pause
 popd
